778560f90c6b204fc5379c8c2eebedd4a43e307e
Lester Ong <lesterong776@gmail.com>
2024-01-20 02:30:47 +0800

    Clean up files

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'CivicTech Lab';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION =
+  'CivicTech Lab at National University of Singapore is a research hub led by Dr. Weiyu Zhang. We are a team of social scientists, computer scientists, and digital cultural analysts.';
