be837173ccbb39406f845f27de6de0d9dc428d34
Lester Ong <lesterong776@gmail.com>
2024-01-23 12:10:46 +0800

    Update sharp config

--- compressImages.cjs
+++ compressImages.cjs
@@ -13,19 +13,19 @@ glob(input_path, function (err, files) {
   if (err != null) {
     throw err;
   }
-  files.forEach(function (inputFile) {
-    sharp(inputFile)
+  files.forEach(async function (inputFile) {
+    let buffer = await sharp(inputFile)
       .jpeg({ quality: 70, force: false })
       .png({ quality: 70, force: false })
-      .toFile(path.join(path.basename(inputFile)), (err, info) => {
-        if (err === null) {
-          fs.unlink(inputFile, (err2) => {
-            if (err2) throw err2;
-            console.log('successfully compressed and deleted ' + inputFile);
-          });
-        } else {
-          throw err;
-        }
-      });
+      .webp({ quality: 70, force: false })
+      .toBuffer();
+
+    sharp(buffer).toFile(path.join(inputFile), (err, info) => {
+      if (err === null) {
+        console.log('Successfully compressed ' + inputFile);
+      } else {
+        throw err;
+      }
+    });
   });
 });
