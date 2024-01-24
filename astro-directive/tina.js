a3378b229d639c1f28a6e3aa152700124fcf71af
Lester Ong <lesterong776@gmail.com>
2024-01-24 15:24:56 +0800

    Add directive for tinacms

---
+++ astro-directive/tina.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('astro').ClientDirective}
+ */
+export default (load, opts, el) => {
+  try {
+    const isEditor = window.frameElement && window.frameElement.id === 'tina-iframe';
+    if (isEditor) {
+      load().then((hydrate) => hydrate());
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
+import clickDirective from './astro-directive/register.js';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [react(), mdx(), sitemap()],
+  integrations: [react(), mdx(), sitemap(), clickDirective()],
 });
