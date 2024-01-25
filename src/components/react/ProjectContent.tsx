0141850f3ee175ddfc89c86b036d06481f50bc9f
Lester Ong <lesterong776@gmail.com>
2024-01-25 17:20:46 +0800

    Populate online deliberation

--- src/components/react/ProjectContent.tsx
+++ src/components/react/ProjectContent.tsx
@@ -8,9 +8,9 @@ const ProjectContent = (props: { query: string; variables: object; data: any }) 
   return (
     <>
       <h1 className="text-center">Project: {title}</h1>
-      <p className="h5 text-center mb-4">{description}</p>
+      {description && <p className="h5 text-center mb-4">{description}</p>}
       {heroImgSrc && (
-        <p>
+        <p className={`${description ? '' : 'mt-4'}`}>
           <img src={heroImgSrc} alt="" />
         </p>
       )}
@@ -20,10 +20,10 @@ const ProjectContent = (props: { query: string; variables: object; data: any }) 
           const template = section.__typename;
           return (
             <div key={section.title}>
-              <h2 className="mt-4 pb-1 border-bottom">{section.title}</h2>
+              <h2 className="mt-4 pb-1 border-bottom mb-3">{section.title}</h2>
               <TinaMarkdown content={section.content}></TinaMarkdown>
               {template === 'ProjectsSectionsPartnerInstitutions' && section.logos && (
-                <div className="d-flex align-items-center justify-content-center flex-wrap row-gap-3 column-gap-5 mt-4">
+                <div className="d-flex align-items-center justify-content-center flex-wrap row-gap-4 column-gap-5 mt-4">
                   {section.logos &&
                     section.logos.map((logo: { name: string; imgSrc: string; link: string }, index: number) => {
                       if (!logo.imgSrc) {
@@ -31,12 +31,23 @@ const ProjectContent = (props: { query: string; variables: object; data: any }) 
                       }
 
                       if (!logo.link) {
-                        return <img style={{ maxWidth: '300px' }} key={logo.name || index} src={logo.imgSrc}></img>;
+                        return (
+                          <img
+                            style={{ maxWidth: '300px', maxHeight: '150px' }}
+                            key={logo.name || index}
+                            src={logo.imgSrc}
+                            alt={logo.name || ''}
+                          />
+                        );
                       }
 
                       return (
                         <a key={logo.name || index} href={logo.link} target="_blank" rel="noopener noreferrer ">
-                          <img src={logo.imgSrc} style={{ maxWidth: '300px' }} />
+                          <img
+                            src={logo.imgSrc}
+                            style={{ maxWidth: '300px', maxHeight: '150px' }}
+                            alt={logo.name || ''}
+                          />
                         </a>
                       );
                     })}
