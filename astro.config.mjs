40742409128985da068c6d105ce835f5dbf5ec95
Lester Ong <lesterong776@gmail.com>
2024-04-20 00:39:33 +0800

    Update site in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import clickDirective from './astro-directive/register.js';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://civictechlab.org',
   integrations: [react(), mdx(), sitemap(), clickDirective()],
 });
