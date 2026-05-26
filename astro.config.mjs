fe59b8c6f0442842f92e9df70222a1867835a6f5
rubberquackingduck <222044594+rubberquackingduck@users.noreply.github.com>
2026-05-27 00:41:43 +0800

    Bump supported node version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 import clickDirective from './astro-directive/register.js';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://civictechlab.org',
-  integrations: [react(), mdx(), sitemap(), clickDirective()],
+  integrations: [react(), mdx(), clickDirective()],
 });
